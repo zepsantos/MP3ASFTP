e9941ad8aa1d70e16258a06ca750d2ebb210173d
SLiFER999 <jogospg1999@gmail.com>
2020-01-03 02:30:26 +0000

    locks

--- src/Server/App.java
+++ src/Server/App.java
@@ -5,6 +5,7 @@ import Models.User;
 
 import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class App {
     private HashMap<String, User> users;
@@ -12,8 +13,10 @@ public class App {
     private AtomicInteger lastID;
     private MusicDatabase musicDatabase;
     private static App inst = null;
+    private ReentrantLock lock;
 
     public App() {
+        this.lock = new ReentrantLock();
         users = new HashMap<>();
         usersByIDSession = new HashMap<>();
         lastID = new AtomicInteger();
@@ -26,30 +29,39 @@ public class App {
     }
 
     public boolean registerUser(String username, String password) {
+        this.lock.lock();
         if(!this.users.containsKey(username)) {
             User user = new User(username,password);
             users.put(username,user);
+            this.lock.unlock();
             return true;
         }
+        this.lock.unlock();
         return false;
 
     }
 
     public User loginUser(String username, String password) { //TODO: Ter cuidado com os locks
+        this.lock.lock();
         if(this.users.containsKey(username)) {
             User user = this.users.get(username);
             if (user.getPassword().equals(password)) {
                 int tmp = lastID.getAndIncrement();
                 user.updateInt(tmp);
-                this.usersByIDSession.put(tmp,user); //NAO PRECISA DE LOCK
+                this.usersByIDSession.put(tmp,user);
+                this.lock.unlock();
                 return user;
+
             }
         }
+        this.lock.unlock();
         return null;
     }
 
     public void  logout(int uid) { //TODO: Ter cuidado com os locks
+        this.lock.lock();
             this.usersByIDSession.remove(uid);
+        this.lock.unlock();
     }
 
 
